e5c0beb41805ea69f678bd0a0c151c5ddd7e7f32
Yash27-95 <yashwanth.ks27@gmail.com>
2025-02-26 16:14:40 +0530

    Initial commit

---
+++ index.js
@@ -0,0 +1,13 @@
+document.addEventListener("click", (e) => {
+    // if(e.target.dataset.featured === "ai" || e.target.id === "ai"){
+    //     // renderAi(e)
+    //     console.log("ai")
+    // }
+    
+    console.log(e.target.id)
+} )
+
+
+function renderAi(e) {
+    console.log(e)
+}
